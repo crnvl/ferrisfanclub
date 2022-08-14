f58868dd84fd5c685a3471350db75fae4dc25ba1
angelsflyinhell <azami@namespace.media>
2022-08-14 21:59:20 +0200

    Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
 fn main() {
-    let arr = [1, 2, 3, 4, 5];
-    print_arr_range(&arr, 0, 3);
-}
-
-fn print_arr_range(arr: &[i32], start: usize, end: usize) {
-    for i in start..end {
-        println!("{}", arr[i]);
-    }
+    println!("Hello, world!");
 }
