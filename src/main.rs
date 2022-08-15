3e5b29a7628e53b0ad0ee654b1a4a382af7994b1
yuki <angelsflyinhell@namespace.media>
2022-08-15 08:20:21 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+/* 
+    create a program that rolls a dice 10 times and checks if the sum is even
+*/
+
+use rand::{Rng}; // 0.8.0
+
 fn main() {
-    println!("Hello, world!");
+    let mut sum = 0;
+    for i in 0..10 {
+        let dice = rand::thread_rng().gen_range(1..7);
+        sum += dice;
+    }
+    if sum % 2 == 0 {
+        println!("The sum is even");
+    } else {
+        println!("The sum is odd");
+    }
 }
