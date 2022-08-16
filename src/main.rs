b9d5d88b16eda5b0934a9a8bed0651ece7a28e11
yuki <angelsflyinhell@namespace.media>
2022-08-16 08:54:26 +0200

    Array merging

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 // write a program that rolls a dice ten times.
 
 fn main() {
-    let mut roll_count = 0;
-    let mut roll_total = 0;
-    while roll_count < 10 {
-        roll_total += rand::thread_rng().gen_range(1, 7);
-        roll_count += 1;
+    const a: [i32; 6] = [1, 2, 3, 4, 5, 6];
+    const b: [i32; 6] = [10, 20, 30, 40, 50, 60];
+
+    let c: [i32; a.len() + b.len()] = merge(&a, &b);
+    println!("{:?}", c);
+}
+
+fn merge(a: &[i32; 6], b: &[i32; 6]) -> [i32; 12] {
+    let mut c: [i32; 12] = [0; 12];
+
+    let mut indexA = 0;
+    let mut indexB = 0;
+
+    for i in 0..12 {
+        if indexA < a.len() && (indexB == b.len() || a[indexA] < b[indexB]) {
+            c[i] = a[indexA];
+            indexA += 1;
+        } else {
+            c[i] = b[indexB];
+            indexB += 1;
+        }
     }
-    println!("Total: {}", roll_total);
+    return c
 }
