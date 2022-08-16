5751a9b2ee105c6810ab0d3f09818eeebd5e275e
yuki <angelsflyinhell@namespace.media>
2022-08-16 11:36:14 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 // write a program that rolls a dice ten times.
 
 fn main() {
-    const a: [i32; 6] = [1, 2, 3, 4, 5, 6];
-    const b: [i32; 6] = [10, 20, 30, 40, 50, 60];
+    const A: [i32; 6] = [1, 2, 3, 4, 5, 6];
+    const B: [i32; 6] = [10, 20, 30, 40, 50, 60];
 
-    let c: [i32; a.len() + b.len()] = merge(&a, &b);
+    let c: [i32; A.len() + B.len()] = merge(&A, &B);
     println!("{:?}", c);
 }
 
 fn merge(a: &[i32; 6], b: &[i32; 6]) -> [i32; 12] {
     let mut c: [i32; 12] = [0; 12];
 
-    let mut indexA = 0;
-    let mut indexB = 0;
+    let mut index_a = 0;
+    let mut index_b = 0;
 
     for i in 0..12 {
-        if indexA < a.len() && (indexB == b.len() || a[indexA] < b[indexB]) {
-            c[i] = a[indexA];
-            indexA += 1;
+        if index_a < a.len() && (index_b == b.len() || a[index_a] < b[index_b]) {
+            c[i] = a[index_a];
+            index_a += 1;
         } else {
-            c[i] = b[indexB];
-            indexB += 1;
+            c[i] = b[index_b];
+            index_b += 1;
         }
     }
     return c
